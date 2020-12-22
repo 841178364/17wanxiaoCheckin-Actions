199332d4cc6d9eca3d446a825a434be44a415d93
shizihanghang <76475675+shizihanghang@users.noreply.github.com>
2020-12-23 07:13:34 +0800

    Update 17wanxiao.py

--- 17wanxiao.py
+++ 17wanxiao.py
@@ -217,20 +217,20 @@ def check_in(username, password):
     post_dict = get_post_json(json1)
     if post_dict:
         # 健康打卡
-        # print(post_dict)
+         print(post_dict)
 
         # 修改温度等参数
-        # for j in post_dict['updatainfo']:  # 这里获取打卡json字段的打卡信息，微信推送的json字段
-        #     if j['propertyname'] == 'temperature':  # 找到propertyname为temperature的字段
-        #         j['value'] = '36.2'  # 由于原先为null，这里直接设置36.2（根据自己学校打卡选项来）
-        #     if j['propertyname'] == '举一反三即可':
-        #         j['value'] = '举一反三即可'
+         for j in post_dict['updatainfo']:  # 这里获取打卡json字段的打卡信息，微信推送的json字段
+             if j['propertyname'] == 'temperature':  # 找到propertyname为temperature的字段
+                 j['value'] = '36.2'  # 由于原先为null，这里直接设置36.2（根据自己学校打卡选项来）
+             if j['propertyname'] == '举一反三即可':
+                 j['value'] = '举一反三即可'
 
         # 修改地址，依照自己完美校园，查一下地址即可
-        # post_dict['areaStr'] = '{"streetNumber":"89号","street":"建设东路","district":"","city":"新乡市","province":"河南省",' \
-        #                        '"town":"","pois":"河南师范大学(东区)","lng":113.91572178314209,' \
-        #                        '"lat":35.327695868943984,"address":"牧野区建设东路89号河南师范大学(东区)","text":"河南省-新乡市",' \
-        #                        '"code":""} '
+         post_dict['areaStr'] = '{"streetNumber":"89号","street":"建设东路","district":"","city":"新乡市","province":"河南省",' \
+                                '"town":"","pois":"河南师范大学(东区)","lng":113.91572178314209,' \
+                                '"lat":35.327695868943984,"address":"牧野区建设东路89号河南师范大学(东区)","text":"河南省-新乡市",' \
+                                '"code":""} '
         healthy_check_dict = healthy_check_in(username, token, post_dict)
         check_dict_list.append(healthy_check_dict)
     else:
